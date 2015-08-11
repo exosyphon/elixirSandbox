b91fb674e9b78705f654bd64fd986d0cc690d914
exosyphon <exosyphon@yahoo.com>
2015-08-11 00:21:52 -0600

    Cleanup code

--- game_of_life.ex
+++ game_of_life.ex
@@ -9,7 +9,7 @@ end
 
 defmodule Output do
   def print_board(board) do
-    Enum.each(0..Enum.count(board)-1, fn(x) -> Enum.each(0..Enum.count(board)-1, fn(y) -> print(board, x, y)  end)  end)
+    Enum.each(0..Board.get_board_size(board), fn(x) -> Enum.each(0..Board.get_board_size(board), fn(y) -> print(board, x, y)  end)  end)
   end
 
   defp print(board, x, y) do
@@ -19,17 +19,21 @@ defmodule Output do
       :io.format "O"
     end
 
-    if(y == Enum.count(board)-1) do
+    board_size = Board.get_board_size(board)
+    if(y == board_size) do
       IO.puts " " 
     end
-    if(x == Enum.count(board)-1 && y == Enum.count(board)-1) do
+    if(x == board_size && y == board_size) do
       IO.puts " " 
     end
-
   end
 end
 
 defmodule Board do
+  def get_board_size(board) do
+    Enum.count(board)-1
+  end
+
   def create_random_board(size) do
     Enum.map(0..size-1, fn(x) -> random_inner_array(size) end)
   end
@@ -39,34 +43,41 @@ defmodule Board do
   end
 
   def applyRules(board) do
-    Enum.map(0..Enum.count(board)-1, fn(x) -> Enum.map(0..Enum.count(board)-1, fn(y) -> check_underpopulation(board, x, y)  end)  end)
+    Enum.map(0..get_board_size(board), fn(x) -> Enum.map(0..get_board_size(board), fn(y) -> check_rules(board, x, y)  end)  end)
   end
  
   def getValue(board, x, y) do
     Enum.at(board, x) |> Enum.at(y)
   end
 
-  defp check_underpopulation(board, x, y) do
-    size = Enum.count(board) - 1
-    items = [[x-1,y],[x-1,y-1],[x-1,y+1],[x,y+1],[x,y-1],[x+1,y+1],[x+1,y],[x+1,y-1]]
-    good_items = Enum.filter(items,fn(x) -> ( (Enum.at(x,0) >= 0 && Enum.at(x,0) <= size ) && (Enum.at(x,1) >= 0 && Enum.at(x,1) <= size ))  end)
-    answer = Enum.filter(good_items, fn(x) -> getValue(board,Enum.at(x,0), Enum.at(x,1)) == true end)
-    (getValue(board, x, y) && (Enum.count(answer) == 2 || Enum.count(answer) == 3) && Enum.count(answer) < 4) || (Enum.count(answer) == 3)
+  defp check_rules(board, x, y) do
+    coordinates = [[x-1,y],[x-1,y-1],[x-1,y+1],[x,y+1],[x,y-1],[x+1,y+1],[x+1,y],[x+1,y-1]]
+    valid_coordinates = Enum.filter(coordinates,fn(x) -> ( (Enum.at(x,0) >= 0 && Enum.at(x,0) <= get_board_size(board) ) && (Enum.at(x,1) >= 0 && Enum.at(x,1) <= get_board_size(board))) end)
+    neighbors_alive = Enum.filter(valid_coordinates, fn(x) -> getValue(board,Enum.at(x,0), Enum.at(x,1)) == true end)
+    cell_dies(board, x, y, neighbors_alive) || cell_reproduces(neighbors_alive)
+  end
+
+  defp cell_reproduces(neighbors_alive) do
+    Enum.count(neighbors_alive) == 3
+  end
+
+  defp cell_dies(board, x, y, neighbors_alive) do
+    getValue(board, x, y) && Enum.count(neighbors_alive) > 1 && Enum.count(neighbors_alive) < 4
   end
 
   def random_inner_array(size) do
     if(:random.uniform >= 0.5) do
-      Enum.map(1..size,fn(_) -> true end)
+      Enum.map(0..size-1, fn(_) -> true end)
     else
-      Enum.map(1..size,fn(_) -> false end)
+      Enum.map(0..size-1, fn(_) -> false end)
     end
   end
 
   defp generate_inner_array(x, size) do
-    if(x==0) do
-      Enum.map(1..size,fn(_) -> true end)
+    if(x == 0) do
+      Enum.map(0..size-1, fn(_) -> true end)
     else
-      Enum.map(1..size,fn(_) -> false end)
+      Enum.map(0..size-1, fn(_) -> false end)
     end
   end
 end
