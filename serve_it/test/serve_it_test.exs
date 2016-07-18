de613c7c85905ca63218462dfd88faa9ad9a8782
exosyphon <exosyphon@yahoo.com>
2016-07-17 21:54:21 -0600

    Add test for users endpoint

--- serve_it/test/serve_it_test.exs
+++ serve_it/test/serve_it_test.exs
@@ -1,7 +1,17 @@
 defmodule ServeItTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+  use Plug.Test
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  @website_router_opts ServeIt.init([])
+  test "returns a user" do
+    ServeIt.Repo.start_link
+    changeset = User.changeset(%User{}, %{id: 1, first_name: "Chase", last_name: "D"})
+    ServeIt.Repo.insert_or_update(changeset)
+    conn = conn(:get, "/users/1")
+    conn = ServeIt.call(conn, @website_router_opts)
+
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert String.match?(conn.resp_body, ~r/Chase/)
   end
 end
